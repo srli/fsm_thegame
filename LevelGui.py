50c19bacc73fa3e3a4a39e9aea06f70619aaf9a0
lvanderlyn <lindsey.vanderlyn@students.olin.edu>
2014-12-11 17:47:27 -0500

    LevelGui.py: made naming convention more consistant Also added lists for states and transitions and their creator functions, at some point, these type of objects should become children of Drag and Drop so they can be initialized slightly differently

--- LevelGui.py
+++ LevelGui.py
@@ -12,7 +12,7 @@ import planes
 
 
 class Drag(planes.Plane):
-    def __init__(self, name, rect, draggable = True, grab = True):
+    def __init__(self, name, rect, draggable=True, grab=True):
         planes.Plane.__init__(self, name, rect, draggable, grab)
         self.name = name
         self.image.fill((255, 0, 0))
@@ -22,8 +22,8 @@ class Drag(planes.Plane):
     def clicked(self, button_name):
         self.image.fill((255,0,0))
 
-class DropZone(planes.Plane):
-    def __init__(self, name, rect, draggable = False, grab = True):
+class Drop(planes.Plane):
+    def __init__(self, name, rect, draggable=False, grab=True, control_enbales=[], next_state=0):
         planes.Plane.__init__(self, name, rect, draggable, grab)
         
         self.name = name
@@ -39,37 +39,49 @@ class DropZone(planes.Plane):
        plane.moving = False
 
 
-
-class DropDisplay(planes.Display):
+class Drop_Display(planes.Display):
 	def dropped_upon(self, plane, coordinates):
-         if isinstance(plane, Drag):
+         if isinstance(plane, Drop):
 #             print plane.Xpos
              planes.Display.dropped_upon(self, plane, (plane.rect.centerx, plane.rect.centery))
 
 class Model():
     def __init__(self):
-        self.drag = []
-        self.dropZones = []
+        self.control_enables = []
+        self.states = []
+        self.transition_conditions = []
+        self.transitions = []
 
-    def makeDrag(self, name, rect):
-        self.drag.append(Drag(name, rect, draggable = True, grab = True))
+
+    def make_control_enables(self, name, rect):
+        self.control_enables.append(Drag(name, rect, draggable = True, grab = True))
+
+    def make_transition_conditions(self, name, rect):
+        self.transition_conditions.append(Drag(name, rect, draggable = True, grab = True))
     
-    def makeDropZone(self, name,rect):
-        drop = DropZone(name, rect, draggable = False, grab = True)
-        self.dropZones.append(drop)
-   
+    def make_states(self, name,rect):
+        drop = Drop(name, rect, draggable = False, grab = True)
+        self.states.append(drop)
+
+    def update_states(self):
+        """
+            updates states to have correct control signals
+        """
+        for state in self.states:
+            pass
+
 class View():
     def __init__(self, model, screen): #View contains model and screen
         self.model = model
         self.screen = screen
         
     def update(self, screen):        
-        for droppy in self.model.dropZones:
+        for droppy in self.model.states:
             this_droppy = droppy
             screen.sub(this_droppy)
             print ("Made a drop zone!")
               
-        for draggy in self.model.drag:
+        for draggy in self.model.control_enables:
             this_draggy = draggy
             screen.sub(this_draggy)                       
             print("Made a draggable")
@@ -78,7 +90,7 @@ class View():
 if __name__ == "__main__":
     running = True    
     pygame.init()
-    screen = DropDisplay((800, 800))    
+    screen = Drop_Display((800, 800))    
     model = Model()
     view = View(model, screen)
     clock = pygame.time.Clock()
@@ -87,12 +99,12 @@ if __name__ == "__main__":
     screen.image.fill((0, 128, 0))
     
     #Make some zones
-    model.makeDropZone("drop1", pygame.Rect(0, 0, 100, 100))
-    model.makeDropZone("drop2", pygame.Rect(700,400, 100,100))    
+    model.make_states("drop1", pygame.Rect(0, 0, 100, 100))
+    model.make_states("drop2", pygame.Rect(700,400, 100,100))    
     
     #Just to test if materials work
     for i in range(0,4):    
-        model.makeDrag("mat"+`i`, pygame.Rect(200, i*75, 50, 50))    
+        model.make_control_enables("mat"+`i`, pygame.Rect(200, i*75, 50, 50))    
 
     view.update(screen)
 
